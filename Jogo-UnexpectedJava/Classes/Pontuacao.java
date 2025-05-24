3274ebef805cc272b34c9a576e6e445a51701c4c
TaxCollectingDemon <maxbrixius@gmail.com>
2025-05-24 18:40:21 -0300

    feat: Classe de pontuação

--- Jogo-UnexpectedJava/Classes/Pontuacao.java
+++ Jogo-UnexpectedJava/Classes/Pontuacao.java
@@ -1,3 +1,51 @@
 public class Pontuacao {
+    int pontos;
+    int acertos;
+    int erros;
+    String titulo;
 
+    public Pontuacao() {
+        this.pontos = 0;
+        this.acertos = 0;
+        this.erros = 0;
+        this.titulo = "";
+    }
+    
+    public int getPontos() {
+        return pontos;
+    }
+
+    public int getAcertos() {
+        return acertos;
+    }
+
+    public int getErros() {
+        return erros;
+    }
+
+    public String getTitulo() {
+        if (pontos >= 0 && pontos <= 200) {
+            titulo = "Novato";
+        } else if (pontos > 200 && pontos <= 350) {
+            titulo = "Aprendiz de Programador";
+        } else if (pontos > 350 && pontos <= 450) {
+            titulo = "Desbravador do Java";
+        } else if (pontos > 30 && pontos <= 40) {
+            titulo = "Mestre do Código";
+        }
+
+        return titulo;
+    }
+
+    public void addPontos(int pontos) {
+        this.pontos += pontos;
+    }
+
+    public void incAcerto() {
+        this.acertos++;
+    }
+
+    public void incErro() {
+        this.erros++;
+    }
 }
